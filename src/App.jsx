4f5aa8cd75221c1a951ddc4dd2ee24db26a26e06
vitorborg <dhastyy1@yahoo.com>
2022-07-28 15:36:48 -0300

    init files project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyle from "./styles/styles";
+
+function App() {
+  return (
+    <>
+      <h1>E-commerce</h1>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/styles.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-family: 'Titillium Web', sans-serif;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    background-color: #fffff;
+  }
+`;
+
+export default GlobalStyle;
